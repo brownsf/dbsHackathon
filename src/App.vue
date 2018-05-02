74f0c46df71d6bc711042a25a7abee0c3754b302
Scott Brown <brownsf@gmail.com>
2018-05-01 20:57:09 -0500

    setups

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,13 @@
   <div id="app">
     <v-app>
       <v-content>
-        <v-container>Hello world</v-container>
+        <v-container>
+          <v-layout row wrap>
+            <v-flex xs12>
+              <div class="headline">Hello world</div>
+      </v-flex>
+      </v-layout>
+      </v-container>
         <router-view/>
       </v-content>
     </v-app>
